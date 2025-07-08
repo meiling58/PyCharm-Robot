491cf9a6ee252d78165c100544a9c5b4d6b793db
Mei-Ling Liang <67974179+meiling58@users.noreply.github.com>
2025-07-08 09:34:15 -0400

    Create setup_local.bat
    adding setup_local.bat file

---
+++ setup_local.bat
@@ -0,0 +1,32 @@
+::Run these commands one time to set global security settings
+::python -m pip config set global.index-url "{url}"
+::python -m pip config set global.trusted-host "{hostName}"
+::python -m pip config set global.user.email "{your_email}"
+::my sample:C:\Users\mliang>git config --global user.email "meiling.liang58@gmail.com"
+
+python -m pip install --upgrade pip
+python -m pip install --upgrade robotframework
+python -m pip install --upgrade robotframework-requests
+python -m pip install --upgrade robotframework-seleniumlibrary
+python -m pip install --upgrade selenium2Library
+python -m pip install --upgrade pxt_robot_common
+python -m pip install --upgrade hrs_robot_common
+python -m pip install --upgrade robotframework-soaplibrary
+python -m pip install --upgrade robotframework-jsonlibrary
+python -m pip install --upgrade robotframework-sshlibrary
+python -m pip install --upgrade robotframework-screencaplibrary
+python -m pip install --upgrade robotframework-excellib
+faker
+openpyxl
+splunk-sdk==1.7.4
+pymongo[srv]
+dnspython
+cx_Oracle
+pytz
+pypdf
+flake8=6.1.0
+requests-auth-aws-sigv4
+pip-system-certs
+boto3
+
+
